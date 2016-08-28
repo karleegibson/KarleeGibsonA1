93333603840ac3be3890f666d123cbcfe6bfc97b
karleegibson <karlee.gibson@my.jcu.edu.au>
2016-08-28 14:05:22 +1000

    Add docstring to top of code file containing name, date, brief program details and a link to project on GitHub. Add draft pseudocode for functions load_items and complete_an_item.

---
+++ KarleeGibsonA1.py
@@ -0,0 +1,21 @@
+""" Name: Karlee Gibson
+    Date: 28/08/16
+    Brief program details: Shopping List 1.0
+    Link to GitHub:
+
+Pseudocode:
+function load_items()
+    display required_items_list
+    display menu choice
+    get menu choice
+
+
+function complete_an_item()
+    display required_items_list
+    get number of item to mark
+    display item marked as completed message
+
+
+
+
+"""
