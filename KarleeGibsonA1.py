0d8325f4dfba94a346e93dcd918a088ea33598cd
karleegibson <karlee.gibson@my.jcu.edu.au>
2016-08-28 20:19:34 +1000

    Create constant 'MENU' and code to open items csv file. Add first three print statements to main, including constant MENU.

--- KarleeGibsonA1.py
+++ KarleeGibsonA1.py
@@ -19,3 +19,18 @@ function complete_an_item()
 
 
 """
+import csv
+
+items_file = open('items.csv', 'r')
+
+MENU = "Menu: \nR - List required items \nC - List completed items \nA - Add new item \nM - Mark an item as " \
+       "completed \nQ - Quit"
+
+
+def main():
+    print("Shopping List 1.0 - by Karlee Gibson")
+    lines = items_file.readlines()
+    print("{} items loaded from items.csv".format(len(lines)))
+    print(MENU)
+
+main()
